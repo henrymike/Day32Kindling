1de2ae80b9e42f928f2761e7643362afb2fa84e5
Mike Henry <chsmike@gmail.com>
2015-11-04 14:47:12 -0500

    Added Login/Sign-up functions from Parse; added Fetch method for Users

---
+++ Kindling/Kindling/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  Kindling
+//
+//  Created by Mike Henry on 11/4/15.
+//  Copyright © 2015 Mike Henry. All rights reserved.
+//
+
+import UIKit
+
+class DataManager: NSObject {
+
+}
